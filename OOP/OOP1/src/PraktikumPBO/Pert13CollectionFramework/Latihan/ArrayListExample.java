ddefd57948a91314931c4144510100014971267c
Chandss28 <dikasoetta16@gmail.com>
2025-07-26 18:13:50 +0700

    Pindah memo

---
+++ OOP/OOP1/src/PraktikumPBO/Pert13CollectionFramework/Latihan/ArrayListExample.java
@@ -0,0 +1,21 @@
+package PraktikumPBO.Pert13CollectionFramework.Latihan;
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<>();
+
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Orange");
+
+        System.out.println("Fruits: " + fruits);
+
+        fruits.remove("Banana");
+        System.out.println("After removing Banana: " + fruits);
+
+        for (String fruit : fruits) {
+            System.out.println("Fruit: " + fruit);
+        }
+    }
+}
